0a795094efec753d3eb8b31e3575d2035e6d63cd
igness1 <sarta98@gmail.com>
2020-04-05 20:06:55 +0200

    Commit z 3 zajęc

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -3,10 +3,43 @@ package com.company;
 public class Human {
 
 
-    String fisrtName;
+    String firstName;
     String lastName;
     Animal pet;
     Phone phone;
-    Car car;
+    private Car car;
+
+    private Double salary = 200.0;
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public void setCar(Car car) {
+        if (car.getValue() <= this.salary) {
+            System.out.println("Great you can buy it by cash");
+            this.car = car;
+        } else if (car.getValue() <= this.salary * 12) {
+            System.out.println("Not great, but you can buy it on credit");
+            this.car = car;
+        } else {
+            System.out.println("Sorry, get a rise");
+        }
+    }
+
+    public Car getCar() {
+        return this.car;
+    }
+
+    public String toString() {
+        return this.firstName + " " + this.lastName;
+    }
+
+
+
 
     }
